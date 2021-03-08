272d1248eb9439f2e42cb06b5cca84110ba892b3
Ammersken Nils <nilsammersken@gmail.com>
2021-03-08 16:18:55 +0100

    Add App.js to storybook, implement GlobalStyle, add fonts

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -5,15 +5,9 @@ export default createGlobalStyle`
     box-sizing: border-box
   }
 
-  @font-face {
-  font-family: "Inter";
-  src: local("Inter"),
-    url("./fonts/Inter.ttf") format("truetype");
-}
-
   body {
     margin: 0;
-    font-family: 'Inter', sans-serif;
+    font-family: sans-serif;
     font-size: 112.5%;
     line-height: 1.5;
     font-weight: 700;
