235d35c52192b898df59e46ebcb1a779726fda0b
Ammersken Nils <nilsammersken@gmail.com>
2021-03-16 16:33:36 +0100

    Add pause button and functionality and update storybook

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components/macro'
 import Countdown from '../Countdown/Countdown'
 import EndTime from '../EndTime/EndTime'
 import { ReactComponent as PlayButton } from '../../assets/play-icon.svg'
+import { ReactComponent as PauseButton } from '../../assets/pause-icon.svg'
 import { ReactComponent as StopButton } from '../../assets/stop-icon.svg'
 import { useState, useEffect } from 'react'
 
@@ -11,6 +12,7 @@ function App() {
 
   const [durationLong, setDurationLong] = useState(false)
   const [isActive, setIsActive] = useState(false)
+  const [isPaused, setIsPaused] = useState(false)
   const [[endHours, endMinutes], setEndTime] = useState([])
   const [timerExpired, setTimerExpired] = useState(false)
   const [[minutes, seconds], setCounter] = useState([
@@ -19,7 +21,7 @@ function App() {
   ])
 
   function timer() {
-    if (timerExpired) return
+    if (isPaused || timerExpired) return
     if (minutes === 0 && seconds === 0) setTimerExpired(true)
     else if (seconds === 0) {
       setCounter([minutes - 1, 59])
@@ -40,14 +42,31 @@ function App() {
     const currentDateObj = new Date()
     const endDateObj = new Date()
 
-    durationLong
-      ? endDateObj.setTime(
-          currentDateObj.getTime() + DURATIONFIFTY.minutes * 60 * 1000
-        )
-      : endDateObj.setTime(
-          currentDateObj.getTime() + DURATIONTWENTYFIVE.minutes * 60 * 1000
-        )
-    setEndTime([endDateObj.getHours(), endDateObj.getMinutes()])
+    if (isPaused) {
+      durationLong
+        ? endDateObj.setTime(
+            currentDateObj.getTime() + (minutes + seconds / 60) * 60 * 1000
+          )
+        : endDateObj.setTime(
+            currentDateObj.getTime() + (minutes + seconds / 60) * 60 * 1000
+          )
+      setEndTime([endDateObj.getHours(), endDateObj.getMinutes()])
+    } else {
+      durationLong
+        ? endDateObj.setTime(
+            currentDateObj.getTime() + DURATIONFIFTY.minutes * 60 * 1000
+          )
+        : endDateObj.setTime(
+            currentDateObj.getTime() + DURATIONTWENTYFIVE.minutes * 60 * 1000
+          )
+      setEndTime([endDateObj.getHours(), endDateObj.getMinutes()])
+    }
+    setIsPaused(false)
+  }
+
+  function handlePause() {
+    setIsPaused(true)
+    setIsActive(false)
   }
 
   function handleStop() {
@@ -62,6 +81,7 @@ function App() {
         ])
     setTimerExpired(false)
     setIsActive(false)
+    setIsPaused(false)
   }
 
   function handleDurationShort() {
@@ -96,13 +116,14 @@ function App() {
           50:00
         </CountdownDuration>
       </ConfigurationGrid>
-      <StartStopGrid>
+      <ExecutionGrid>
+        <StopButton role="button" onClick={handleStop} />
         {!isActive ? (
           <PlayButton role="button" onClick={handleStart} />
         ) : (
-          <StopButton role="button" onClick={handleStop} />
+          <PauseButton role="button" onClick={handlePause} />
         )}
-      </StartStopGrid>
+      </ExecutionGrid>
     </AppGrid>
   )
 }
@@ -111,12 +132,12 @@ export default App
 
 const AppGrid = styled.main`
   display: grid;
+  grid-template-rows: 1fr 1fr 100px;
   position: fixed;
   left: 0;
   top: 0;
   width: 100%;
   height: 100%;
-  grid-template-rows: 1fr 1fr 100px;
 `
 
 const CountdownGrid = styled.section`
@@ -130,14 +151,16 @@ const CountdownGrid = styled.section`
 const ConfigurationGrid = styled.section`
   display: grid;
   grid-template-columns: auto auto;
+  gap: 10px;
+  margin: 0 0 20px;
   align-content: end;
   justify-content: center;
-  gap: 10px;
-  padding: 20px;
 `
 
-const StartStopGrid = styled.section`
+const ExecutionGrid = styled.section`
   display: grid;
+  grid-template-columns: auto auto;
+  gap: 40px;
   align-content: start;
   justify-content: center;
   padding: 10px;
