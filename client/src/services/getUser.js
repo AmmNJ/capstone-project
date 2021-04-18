2345c8887b436a8590bf73aa8df516cfa2d9dfd9
Ammersken Nils <nilsammersken@gmail.com>
2021-04-18 17:03:57 +0200

    Remove unused files and functions and merge simplify createChartData function

--- client/src/services/getUser.js
+++
@@ -1,3 +0,0 @@
-export default function getUser(name) {
-  return fetch(`'/api/users/${name}'`).then(res => res.json())
-}
--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,6 @@ const express = require('express')
 const User = require('../models/User')
 const router = express.Router()
 
-router.get('/:name', async (req, res, next) => {
-  const { name } = req.params
-  res.json(await User.find({ name: name }).catch(next))
-})
-
 router.post('/', async (req, res, next) => {
   res.json(await User.create(req.body).catch(next))
 })
